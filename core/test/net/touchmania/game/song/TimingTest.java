8e27dfb46570f19571302c35f520b7070b1d156f
flood2d <vinsfortunato97@gmail.com>
2019-09-30 17:43:36 +0200

    Add test functionality

---
+++ core/test/net/touchmania/game/song/TimingTest.java
@@ -0,0 +1,12 @@
+package net.touchmania.game.song;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TimingTest {
+
+    @Test
+    public void testTiming() {
+        assertEquals(1, 1);
+    }
+}
